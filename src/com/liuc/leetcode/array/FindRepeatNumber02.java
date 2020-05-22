c17df42250ee237d8b9ecf500990182decce3136
liuc <liuc@systec.com.cn>
2020-05-22 10:29:19 +0800

    面试题03. 数组中重复的数字

---
+++ src/com/liuc/leetcode/array/FindRepeatNumber02.java
@@ -0,0 +1,40 @@
+package com.liuc.leetcode.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 面试题03. 数组中重复的数字
+ * 找出数组中重复的数字。
+ * <p>
+ * 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：
+ * [2, 3, 1, 0, 2, 5, 3]
+ * 输出：2 或 3
+ * <p>
+ * 限制：
+ * 2 <= n <= 100000
+ */
+public class FindRepeatNumber02 {
+    // 解法二：如果主要考虑空间复杂度的话，空间复杂度O(1)，要使用鸽巢原理，剑指offer上也推荐
+    public int findRepeatNumber(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] != i) {
+                if (nums[nums[i]] == nums[i]) return nums[i];
+                int tmp = nums[i];
+                nums[i] = nums[tmp];
+                nums[tmp] = tmp;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 0, 2, 5, 3};
+        int result = new FindRepeatNumber02().findRepeatNumber(nums);
+        System.out.println();
+    }
+}
